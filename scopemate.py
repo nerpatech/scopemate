b72314bf3090ea9c095c7668745e73fbbca0ba8b
user <user@host>
2024-11-11 15:43:49 -0700

    Applied some improvements suggested by Claude

--- scopemate.py
+++ scopemate.py
@@ -16,7 +16,9 @@ from io import BytesIO
 from PIL import Image
 import pyvisa
 
-default_mask = ['mask-default-blank.png']
+CHUNK_SIZE = 1420
+TIMEOUT = 30000
+DEFAULT_MASK = ['mask-default-blank.png']
 do_clean = False
 do_sync = False
 
@@ -24,22 +26,22 @@ parser = argparse.ArgumentParser()
 rm = pyvisa.ResourceManager()
 
 def get_screen(resource, filename, masks):
-    instr = rm.open_resource(resource)
-    instr.timeout = 30000
-    instr.chunk_size = 1420     # magic number for faster speed
-    name = filename + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.png'
-    if do_clean:
-       clean_screen(instr)
-    if do_sync:
-        sync_time(instr)
-    instr.write(':DISPlay:DATA?')
-    screenshot = instr.read_raw()[11:] # discard BMP header
-    im = Image.open(BytesIO(screenshot))
-    im.putalpha(255)
-    for mask in masks:
-        overlay = Image.open(mask)
-        im = Image.alpha_composite(im, overlay)
-    im.save(name)
+    with rm.open_resource(resource) as instr:
+        instr.timeout = TIMEOUT
+        instr.chunk_size = CHUNK_SIZE
+        name = filename + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '.png'
+        if do_clean:
+            clean_screen(instr)
+        if do_sync:
+            sync_time(instr)
+        instr.write(':DISPlay:DATA?')
+        screenshot = instr.read_raw()[11:] # discard BMP header
+        im = Image.open(BytesIO(screenshot))
+        im.putalpha(255)
+        for mask in masks:
+            overlay = Image.open(mask)
+            im = Image.alpha_composite(im, overlay)
+        im.save(name)
     instr.close()
 
 # 'instr' must be open before calling this function 
@@ -56,44 +58,49 @@ def sync_time(instr):
     instr.query('SYSTem:TIME ' + date_time[1] + ';*OPC?')
     print("Oscilloscope clock set to PC time")
 
-parser.add_argument("-i", "--instrument", help="instrument to query")
-parser.add_argument("-l", "--list", help="list available instruments",
+def main():
+    parser.add_argument("-i", "--instrument", help="instrument to query")
+    parser.add_argument("-l", "--list", help="list available instruments",
                     action='store_true')
-parser.add_argument("-m", "--mask", nargs='*', help="apply mask(s)")
-parser.add_argument("-o", "--output", help="output filename prefix")
-parser.add_argument("-s", "--synchronize", help="sync instrument's \
+    parser.add_argument("-m", "--mask", nargs='*', help="apply mask(s)")
+    parser.add_argument("-o", "--output", help="output filename prefix")
+    parser.add_argument("-s", "--synchronize", help="sync instrument's \
                     date and time with a PC", action="store_true")
-parser.add_argument("-c", "--clean", help="turn off the \
+    parser.add_argument("-c", "--clean", help="turn off the \
                     measurements at the bottom of the screen",
                     action="store_true")
-
-args = parser.parse_args()
-
-# if -l is specified all other parameters are silently ignored
-if args.list:
-    print(rm.list_resources())
-    exit(0)
-
-if args.instrument:
-    if args.output:
-        filename = args.output
-    else:
-        filename = 'screenshot'
-
-    if args.mask:
-        pass
-    elif args.mask is None:  # New condition for no -m parameter
-        args.mask = []       # Empty list means no masks
-    else: 
-        args.mask = default_mask
-
-    if args.clean:
-        do_clean = True
-
-    if args.synchronize:
-        do_sync = True    
+    args = parser.parse_args()
     
-    get_screen(args.instrument, filename, args.mask)
-    exit(0)
+    # if -l is specified all other parameters are silently ignored
+    if args.list:
+        print(rm.list_resources())
+        return
 
-print("Nothing to do. Use '-h' to get help")
+    if args.instrument:
+        if args.output:
+            filename = args.output
+        else:
+            filename = 'screenshot'
+
+        if args.mask:
+            pass
+        elif args.mask is None:  # New condition for no -m parameter
+            args.mask = []       # Empty list means no masks
+        else: 
+            args.mask = DEFAULT_MASK
+
+        if args.clean:
+            global do_clean
+            do_clean = True
+
+        if args.synchronize:
+            global do_sync
+            do_sync = True    
+        
+        get_screen(args.instrument, filename, args.mask)
+        return
+
+    print("Nothing to do. Use '-h' to get help")
+
+if __name__ == "__main__":
+    main()
